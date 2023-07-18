7c50ef3a18ca6ed48a20b7b3a1661cbd53522065
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-07-18 01:55:11 -0300

    bloqueio de paginas, login e logout

--- filme/urls.py
+++ filme/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from filme import views
+from django.contrib.auth import views as auth_view
 
-app_name= 'filme'
+app_name = 'filme'
 
 urlpatterns = [
-        path('', views.Homepage.as_view(), name='homepage' ),
-        path('filmes/', views.Homefilmes.as_view(), name='homefilmes'),
-        path('filmes/<int:pk>', views.Detalhesfilme.as_view(), name='detalhesfilme'),
-        path('pesquisa/', views.Pesquisa_filme.as_view(), name='pesquisa_filme')
-        ]
+    path('', views.Homepage.as_view(), name='homepage'),
+    path('filmes/', views.Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', views.Detalhesfilme.as_view(), name='detalhesfilme'),
+    path('pesquisa/', views.Pesquisa_filme.as_view(), name='pesquisa_filme'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
+]
--- filme/views.py
+++ filme/views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # def homepage(request):
@@ -20,36 +21,41 @@ class Homepage(TemplateView):
 #     context['lista_filmes'] = lista_filmes
 #     return render(request, "homefilmes.html", context)
 
-class Homefilmes(ListView):
+class Homefilmes(LoginRequiredMixin, ListView):
     template_name = "homefilmes.html"
     model = Filme
 
-class Detalhesfilme(DetailView):
+
+class Detalhesfilme(LoginRequiredMixin, DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
 
     def get_context_data(self, **kwargs):
         context = super(Detalhesfilme, self).get_context_data()
-        filmes_relacionados = self.model.objects.filter(categoria=self.get_object().categoria)[0:5]
+        filmes_relacionados = self.model.objects.filter(
+            categoria=self.get_object().categoria)[0:5]
         context['filmes_relacionados'] = filmes_relacionados
         return context
-    
+
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any):
         # que filme o usuario esta a acessando?
-        filme  = self.get_object()
+        filme = self.get_object()
         filme.vizualizacoes += 1
         filme.save()
+        usuario = request.user
+        usuario.filmes_visualizados.add(filme)
         return super().get(request, *args, **kwargs)
 
 
-class Pesquisa_filme(ListView):
+class Pesquisa_filme(LoginRequiredMixin, ListView):
     template_name = "pesquisa.html"
     model = Filme
 
     def get_queryset(self):
         termo_pesquisa = self.request.GET.get('query')
         if termo_pesquisa:
-            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa)
+            object_list = self.model.objects.filter(
+                titulo__icontains=termo_pesquisa)
             return object_list
         else:
             return None
