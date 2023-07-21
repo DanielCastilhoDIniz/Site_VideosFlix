12d4c20d3f020ae7e17d734a1d7ac3fc4421aa8b
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-07-21 00:32:15 -0300

    deploy in vercel

--- siteflix/settings.py
+++ siteflix/settings.py
@@ -90,11 +90,11 @@ import dj_database_url
 import os
 
 
-DATABASE_URL =os.getenv("DATABASE_URL")
-if DATABASE_URL:
-    DATABASE = {
-        'default':dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-    }
+# DATABASE_URL =os.getenv("DATABASE_URL")
+# if DATABASE_URL:
+#     DATABASE = {
+#         'default':dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+#     }
 
 
 # Password validation
